6aeeafea2d676799ffcbb802696a7b150ef75f49
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-11-09 20:50:32 +0000

    Init Project

---
+++ src/main/java/com/budget_bees/budget_bees_api/BudgetBeesApiApplication.java
@@ -0,0 +1,13 @@
+package com.budget_bees.budget_bees_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBeesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBeesApiApplication.class, args);
+	}
+
+}
